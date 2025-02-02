bd763fe35aac1d59be719830000b127021f05ad8
saranmoto <saran.moto@gmail.com>
2025-02-02 22:02:06 +0530

    build 3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,15 +4,15 @@ test('has title', async ({ page }) => {
   await page.goto('https://google.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Google/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://google.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // // Click the get started link.
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
