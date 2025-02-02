81120a902140dc2b23fa6e7107182ce7c741b643
saranmoto <saran.moto@gmail.com>
2025-02-02 21:55:10 +0530

    build 2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from "@chromatic-com/playwright";
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://google.com/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://google.com/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
